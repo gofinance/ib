7ea86c2f32f68dbc2f7b3a3cd152e549155cab3b
Joel Reymont <joelr1@gmail.com>
2012-08-01 13:14:53 +0100

    string test passes

--- ibtws/wire/wire_test.go
+++ ibtws/wire/wire_test.go
@@ -9,6 +9,10 @@ type intRec struct {
 	I int64
 }
 
+type stringRec struct {
+    S string
+}
+
 func makebuf() *bytes.Buffer {
 	return bytes.NewBuffer(make([]byte, 0, 1024))
 }
@@ -20,4 +24,13 @@ func TestEncodeInt(t *testing.T) {
 	if b.String() != "57\000" {
 		t.Errorf("encode(%v) = %s, want %s", v, b.String(), "57")
 	}
+}
+
+func TestEncodeString(t *testing.T) {
+    v := &stringRec{S:"foobar" }
+    b := makebuf()
+    encode(b, 0, v)
+    if b.String() != "foobar\000" {
+        t.Errorf("encode(%v) = %s, want %s", v, b.String(), "foobar")
+    }
 }
