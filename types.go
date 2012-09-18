d388d64d1eca3683f971dbe01c27480880024a42
Joel Reymont <joelr1@gmail.com>
2012-09-18 08:58:08 -0400

    easier lookup of tick types

--- types.go
+++ types.go
@@ -85,65 +85,65 @@ const (
 const maxInt = int(^uint(0) >> 1)
 
 const (
-	TickBidSize int64 = iota
-	TickBid
-	TickAsk
-	TickAskSize
-	TickLast
-	TickLastSize
-	TickHigh
-	TickLow
-	TickVolume
-	TickClose
-	TickBidOptionComputation
-	TickAskOptionComputation
-	TickLastOptionComputation
-	TickModelOption
-	TickOpen
-	TickLow13Week
-	TickHigh13Week
-	TickLow26Week
-	TickHigh26Week
-	TickLow52Week
-	TickHigh52Week
-	TickAverageVolume
-	TickOpenInterest
-	TickOptionHistoricalVol
-	TickOptionImpliedVol
-	TickOptionBidExch
-	TickOptionAskExch
-	TickOptionCallOpenInt
-	TickOptionPutOpenInt
-	TickOptionCallVolume
-	TickOptionPutVolume
-	TickIndexFuturePremium
-	TickBidExch
-	TickAskExch
-	TickAuctionVolume
-	TickAuctionPrice
-	TickAuctionImbalance
-	TickMarkPrice
-	TickBidEFPComputation
-	TickAskEFPComputation
-	TickLastEFPComputation
-	TickOpenEFPComputation
-	TickHighEFPComputation
-	TickLowEFPComputation
-	TickCloseEFPComputation
-	TickLastTimestamp
-	TickShortable
-	TickFundamentalRations
-	TickRTVolume
-	TickHalted
-	TickBidYield
-	TickAskYield
-	TickLastYield
-	TickCustOptionComputation
-	TickTradeCount
-	TickTradeRate
-	TickVolumeRate
-	TickLastRTHTrade
-	TickNotSet
+	TickBidSize               = 0
+	TickBid                   = 1
+	TickAsk                   = 2
+	TickAskSize               = 3
+	TickLast                  = 4
+	TickLastSize              = 5
+	TickHigh                  = 6
+	TickLow                   = 7
+	TickVolume                = 8
+	TickClose                 = 9
+	TickBidOptionComputation  = 10
+	TickAskOptionComputation  = 11
+	TickLastOptionComputation = 12
+	TickModelOption           = 13
+	TickOpen                  = 14
+	TickLow13Week             = 15
+	TickHigh13Week            = 16
+	TickLow26Week             = 17
+	TickHigh26Week            = 18
+	TickLow52Week             = 19
+	TickHigh52Week            = 20
+	TickAverageVolume         = 21
+	TickOpenInterest          = 22
+	TickOptionHistoricalVol   = 23
+	TickOptionImpliedVol      = 24
+	TickOptionBidExch         = 25
+	TickOptionAskExch         = 26
+	TickOptionCallOpenInt     = 27
+	TickOptionPutOpenInt      = 28
+	TickOptionCallVolume      = 29
+	TickOptionPutVolume       = 30
+	TickIndexFuturePremium    = 31
+	TickBidExch               = 32
+	TickAskExch               = 33
+	TickAuctionVolume         = 34
+	TickAuctionPrice          = 35
+	TickAuctionImbalance      = 36
+	TickMarkPrice             = 37
+	TickBidEFPComputation     = 38
+	TickAskEFPComputation     = 39
+	TickLastEFPComputation    = 40
+	TickOpenEFPComputation    = 41
+	TickHighEFPComputation    = 42
+	TickLowEFPComputation     = 43
+	TickCloseEFPComputation   = 44
+	TickLastTimestamp         = 45
+	TickShortable             = 46
+	TickFundamentalRations    = 47
+	TickRTVolume              = 48
+	TickHalted                = 49
+	TickBidYield              = 50
+	TickAskYield              = 51
+	TickLastYield             = 52
+	TickCustOptionComputation = 53
+	TickTradeCount            = 54
+	TickTradeRate             = 55
+	TickVolumeRate            = 56
+	TickLastRTHTrade          = 57
+	TickNotSet                = 58
 )
 
 type Request interface {
